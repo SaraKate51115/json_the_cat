c9b486a5d4e16544a6a1275966032a732d51338d
SaraKate51115 <sarakate51115@gmail.com>
2020-11-07 23:15:05 +0000

    refactored code and added unit testing

--- w2/d4/breedFetcher.js
+++ w2/d4/breedFetcher.js
@@ -1,18 +1,24 @@
 const request = require('request');
-const input = process.argv.slice(2);
-const inputBreed = input.toString();
-const breedURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + inputBreed;
 
-request(breedURL, (error, response, body) => {
+const fetchBreedDescription = function(inputBreed, callback) {
+  const breedURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + inputBreed;
+
+  request(breedURL, (error, response, body) => {
   
-  if (error) throw error;
+    if (error) throw error;
+  
+    const data = JSON.parse(body);
+    //console.log(data)
 
-  const data = JSON.parse(body);
-  console.log(data[0].name);
-  console.log(data[0].description);
-});
+    let breedInfo = data[0];
 
+    if (breedInfo) {
+      callback(null, breedInfo.description);
+    } else {
+      callback('no breed');
+    }
+    
+  });
+};
 
-//TEST CASES:
-//requested breed not found
-// && request failed
+module.exports = { fetchBreedDescription };
